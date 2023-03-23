015333722c21fb8b8e12329cb782b2efa05a52de
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-23 17:40:05 -0300

    document context

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript ,DocumentContext} from "next/document";
 import { CssBaseline } from "@nextui-org/react";
 import React from "react";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx:) {
+  static async getInitialProps(ctx:DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
