e044e0485f062608db506a6e1a073a0754d88453
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-27 13:36:35 -0300

    sprites,search another request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,47 @@
-import { Layout } from "../components/layout/Layout";
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next"
+import { Layout } from '../components/layout/Layout';
+
+import { NextPage, GetStaticProps } from "next"
+import  axios  from 'axios';
+import { PokemonListResponse, SmallPokemon } from '../interfaces/pokemon-list';
+
+interface HomePageProps  {
+  pokemons: SmallPokemon[];
+}
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage <HomePageProps>= ({pokemons}:HomePageProps) => {
+  console.log(pokemons);
+  
   return (
     <>
       <Layout title="Listado">
 
-        <Button color={'gradient'}>
-          Next
-        </Button>
+       {/* <ul>
+       {pokemons.map((pokemon: SmallPokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+          
+        
+       </ul> */}
 
       </Layout>
 
     </>
   )
+}
+//funcion que se ejecuta del lado del servidor en tiempo de construccion de la aplicacion
+export const getStaticProps:GetStaticProps = async (ctx) => {
+
+  const pokeApi = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2'
+  })
+
+  const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+console.log(data);
+
+  return {
+    props:{ pokemons:data}
+  }
 }
 
 
