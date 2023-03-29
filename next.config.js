7880a033694add04ba5cf6e6dd3fe209b78dcbbe
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-28 22:21:00 -0300

    undefined array

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['raw.githubusercontent.com']
+    domains: ['raw.githubusercontent.com','pokemonapi.com',]
   },
+  
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,59 @@
 import { Layout } from '../components/layout/Layout';
-
 import { NextPage, GetStaticProps } from "next"
-import  axios  from 'axios';
+import axios from 'axios';
 import { PokemonListResponse, SmallPokemon } from '../interfaces/pokemon-list';
+import Image from 'next/image';
 
-interface HomePageProps  {
+interface HomePageProps {
   pokemons: SmallPokemon[];
 }
 
 
-const HomePage: NextPage <HomePageProps>= ({pokemons}:HomePageProps) => {
-  console.log(pokemons);
-  
+const HomePage: NextPage<HomePageProps> = ({ pokemons }: HomePageProps) => {
+
+
   return (
     <>
       <Layout title="Listado">
-
-       {/* <ul>
-       {pokemons.map((pokemon: SmallPokemon) => (
-            <li key={pokemon.name}>{pokemon.name}</li>
+        <ul>
+          {pokemons.map((pokemon: SmallPokemon) => (
+            <li key={pokemon.name}>{pokemon.url} </li>
           ))}
-          
-        
-       </ul> */}
-
+        </ul>
       </Layout>
-
     </>
   )
 }
 //funcion que se ejecuta del lado del servidor en tiempo de construccion de la aplicacion
-export const getStaticProps:GetStaticProps = async (ctx) => {
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
 
   const pokeApi = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2'
+    baseURL: 'https://pokeapi.co/api/v2',
   })
 
-  const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
-console.log(data);
+  const { data } = await pokeApi.get<PokemonListResponse>("/ability/?limit=20&offset=20")
 
+  const id= data.results.map(i=>{
+    {
+      name: i.name;
+      url: i.url
+    }
+
+
+  })
+  console.log(id);
+
+
+
+  //  const url = await pokeApi.get<PokemonListResponse>(`/https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`)
+
+  // const resp = await pokeApi.get<PokemonListResponse>(`/pokemon/${id}}`)
+  
   return {
-    props:{ pokemons:data}
+    props: {
+      pokemons: data.results,
+    }
   }
 }
 
